6e05ca03e445362e754a755a335a6f657a03f77d
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2021-05-05 02:03:44 +0900

    feat: フロントでも関連記事を取得し表示する

--- app/pages/bookmarks/[id]/index.tsx
+++ app/pages/bookmarks/[id]/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import styles from '../../../styles/Home.module.css'
 import { OgpBox } from '../../../components/OgpBox'
@@ -9,17 +10,30 @@ export default function Details() {
   const { id } = router.query
 
   const [bookmark, setBookmark] = useState<any>(undefined);
+  const [relatedBookmark, setRelated] = useState<Array<any>>([]);
   const [isLoading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     if(!Number(id)) return
     const f = async () => {
-      const bookmark = await getBookmarkByIdAsync(Number(id));
-      setBookmark(bookmark)
+      const res = await getBookmarkByIdAsync(Number(id));
+      setBookmark(res.bookmark)
+      setRelated(res.related)
       setLoading(false)
     }
     f();
   }, [id])
+
+  const RelatedBookmarkList = relatedBookmark.map((item: any) => {
+    return item.ogp.hasOwnProperty('og:title')?
+    (<Link href="/bookmarks/[id]" as={`/bookmarks/${item.id}`} key={item.id}>
+      <a>
+        <OgpBox ogp={item.ogp} />
+      </a>
+    </Link>)
+    : null
+  })
+
 
   return (
     <div>
@@ -30,8 +44,10 @@ export default function Details() {
           null :
           <div className={styles.content}>
             <OgpBox ogp={bookmark.ogp} />
-            <p>memo</p>
+            <p>[memo]</p>
             <p>{bookmark.memo}</p>
+            <hr />
+            {RelatedBookmarkList}
           </div>
         }
       </main>
